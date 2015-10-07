07500f6571ed8d43f51e5840950b3949775872dc
petun <petun911@gmail.com>
2015-10-08 00:56:44 +0300

    Add bower packages to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    bower = require('gulp-bower'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
@@ -13,6 +14,12 @@ var gulp = require('gulp'),
     del = require('del');
 
 
+
+gulp.task('bower', function() {
+    return bower()
+        .pipe(gulp.dest('lib/'))
+});
+
 gulp.task('styles', function() {
     return sass('src/scss/main.scss', { style: 'expanded' })
         .pipe(autoprefixer('last 2 version'))
@@ -24,7 +31,7 @@ gulp.task('styles', function() {
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src(['src/js/**/*.js'])
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
@@ -36,4 +43,4 @@ gulp.task('scripts', function() {
 });
 
 
-gulp.task('default', ['styles', 'scripts']);
+gulp.task('default', ['styles', 'scripts', 'bower']);
