79a0fc5d1b3c90efa2a5a47c21bd0c2ef4833a35
petun <petun911@gmail.com>
2015-10-09 21:59:40 +0300

    Add bat file for bower and update some packages.

---
+++ bower.bat
@@ -0,0 +1,11 @@
+@echo off
+
+rem -------------------------------------------------------------
+rem  command line script for Windows.
+rem -------------------------------------------------------------
+
+@setlocal
+
+node "node_modules\bower\bin\bower" %*
+
+@endlocal
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
@@ -29,6 +30,7 @@ gulp.task('styles', function() {
         .pipe(rename({suffix: '.min'}))
         .pipe(minifycss())
         .pipe(gulp.dest('dist/css'))
+        .pipe(livereload())
         .pipe(notify({ message: 'Styles task complete' }));
 });
 
@@ -51,11 +53,26 @@ gulp.task('slim', function() {
             require: 'slim/include',
             options: 'include_dirs=[".", "src/slim/blocks"]'
         }))
-        .pipe(gulp.dest("./dist/"));
+        .pipe(gulp.dest("./dist/"))
+        .pipe(livereload());
+});
+
+
+gulp.task('images', function() {
+    return gulp.src('src/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('watch', function () {
+    livereload.listen();
     gulp.watch('./src/scss/**/*.scss', ['styles']);
+    gulp.watch('./src/slim/**/*.slim', ['slim']);
+    gulp.watch('./src/js/**/*.js', ['scripts']);
 });
 
 
